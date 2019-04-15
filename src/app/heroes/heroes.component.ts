6b13f590a18456f13421100bcbb7dfaf6ef99adc
mfr <marc.frauenfelder@adesso.ch>
2019-04-15 16:28:13 +0200

    Added routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import { HeroService } from '../hero.service';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +10,9 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  constructor(private heroService: HeroService) {
   }
-  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
